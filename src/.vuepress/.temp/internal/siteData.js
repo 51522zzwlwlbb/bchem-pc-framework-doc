6ea8a5fe4cbba43af2510abca2c272c69a5311a7
炒饭大师干饭达人恋爱废柴健身呆头鹅 <20981116+51522zzwlwlbb@users.noreply.github.com>
2023-09-08 15:05:04 +0800

    feat:修改base

--- src/.vuepress/.temp/internal/siteData.js
+++ src/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"HGB-Admin项目文档 V1.0.0\",\"description\":\"化工宝前端框架 HGB-Admin 的项目文档\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/bchem-pc-framework-doc/\",\"lang\":\"zh-CN\",\"title\":\"HGB-Admin项目文档 V1.0.0\",\"description\":\"化工宝前端框架 HGB-Admin 的项目文档\",\"head\":[],\"locales\":{}}")
--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress';
 import theme from './theme.js';
 
 export default defineUserConfig({
-	base: '/',
+	base: '/bchem-pc-framework-doc/',
 	lang: 'zh-CN',
 	title: 'HGB-Admin项目文档 V1.0.0',
 	description: '化工宝前端框架 HGB-Admin 的项目文档',
